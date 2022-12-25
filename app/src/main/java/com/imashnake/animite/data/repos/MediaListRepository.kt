10629d86d0033ac6fbd79c5de93609ca8291ce74
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-12-25 18:07:34 +1300

    Update MediaListRepository to use the db cache

--- app/src/main/java/com/imashnake/animite/data/repos/MediaListRepository.kt
+++ app/src/main/java/com/imashnake/animite/data/repos/MediaListRepository.kt
@@ -1,29 +1,49 @@
 package com.imashnake.animite.data.repos
 
-import com.imashnake.animite.MediaListQuery
+import com.imashnake.animite.data.repos.mappers.MediaMapper
+import com.imashnake.animite.data.sauce.db.medialist.MediaListDatabaseSource
+import com.imashnake.animite.data.sauce.db.model.ListTag
+import com.imashnake.animite.data.sauce.db.model.Media
 import com.imashnake.animite.data.sauce.network.MediaListNetworkSource
 import com.imashnake.animite.type.MediaSeason
 import com.imashnake.animite.type.MediaSort
 import com.imashnake.animite.type.MediaType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class MediaListRepository @Inject constructor(
-    private val mediaListNetworkSource: MediaListNetworkSource
+    private val mediaListNetworkSource: MediaListNetworkSource,
+    private val mediaListDatabaseSource: MediaListDatabaseSource
 ) {
-    suspend fun fetchMediaList(
+
+    fun getMediaList(
+        tag: ListTag,
         mediaType: MediaType,
-        page: Int,
-        perPage: Int,
         sort: List<MediaSort>,
-        season: MediaSeason?,
-        seasonYear: Int?
-    ): MediaListQuery.Page? =
-        mediaListNetworkSource.fetchMediaList(
-            mediaType = mediaType,
-            page = page,
-            perPage = perPage,
-            sort = sort,
-            season = season,
-            seasonYear = seasonYear
-        )
+        page: Int = 0,
+        perPage: Int = 10,
+        season: MediaSeason? = null,
+        seasonYear: Int? = null
+    ): Flow<Result<List<Media>>> =
+        mediaListDatabaseSource.getMedia(mediaType, tag)
+            .onStart {
+                mediaListDatabaseSource.insertMedia(
+                    mediaListNetworkSource.fetchMediaList(
+                        mediaType, page, perPage, sort, season, seasonYear
+                    )?.media
+                        .orEmpty()
+                        .filterNotNull()
+                        .map(MediaMapper::mediaApiToDB),
+                    tag
+                )
+            }
+            .map {
+                Result.success(it)
+            }
+            .catch {
+                emit(Result.failure(it))
+            }
 }
